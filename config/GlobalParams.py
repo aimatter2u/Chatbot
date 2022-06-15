7f9c836b0614a9192149f22871e3ba3286971cb8
DevYeop <lee.sangyeop0715@gmail.com>
2022-06-15 14:32:17 +0900

    first commit

---
+++ config/GlobalParams.py
@@ -0,0 +1,9 @@
+"""
+챗봇 엔진에서 사용할 전역 파라미터 정보입니다.
+"""
+
+# 단어 시퀀스 벡터 크기
+MAX_SEQ_LEN = 15
+
+def GlobalParams():
+    global MAX_SEQ_LEN
---
+++ train_tools/dict/create_dict.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from utils.Preprocess import Preprocess
+from tensorflow.keras import preprocessing
+import pickle
+
+
+def read_corpus_data(filename):
+    with open(filename, 'r', "utf-8") as f:
+        data = [line.split('\t') for line in f.read().splitlines()]
+    return data
+
+
+corpus_data = read_corpus_data('./corpus.txt')
+
+p = Preprocess(word2index_dic='chatbot_dict.bin',
+               userdic='../utils/user_dic.tsv')
+dict = []
+for c in corpus_data:
+
+    pos = p.pos(c[1])
+    for k in pos:
+        print('k', k)
+
+        dict.append(k[0])
+
+tokenizer = preprocessing.text.Tokenizer(oov_token='OOV')
+tokenizer.fit_on_texts(dict)
+word_index = tokenizer.word_index
+
+f = open("chatbot_dict.bin", "wb")
+try:
+    pickle.dump(word_index, f)
+except Exception as e:
+    print(e)
+finally:
+    f.close()
